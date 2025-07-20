625c256c5c88fa5ead027bcd022a7eb3a9365d33
vinsondm <96635563+vinsondm@users.noreply.github.com>
2025-07-20 00:03:50 -0500

    Add files via upload

---
+++ vinson_person_information.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : vinson_person_info.cpp
+// Author      : David Vinson
+// Description : Information dump for person, set in program
+// Date Created: 07-19-2025
+//============================================================================/*
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Fictional person information
+    string firstName = "Dave";
+    string lastName = "Vinson";
+    string streetAddress = "1234 Firefly Street";
+    string city = "Serenity, IL";
+    string zipCode = "98765";
+
+    // Display the information
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
